dda818830356d2db03b331c3a0e1da00135f642d
Florence-wangui <florencenyokabiwangui@gmail.com>
2023-05-09 08:19:09 +0300

    sync

---
+++ assignment.js
@@ -0,0 +1,15 @@
+// Write an asynchronous function that accepts a message string and a delay time in milliseconds. 
+// The function should log the message to the console after the specified delay time.
+function name(params) {
+    
+}
+
+// You have an array of user IDs and a function getUserData(id) that returns a Promise with user
+//  data when given a user ID. Write an asynchronous function that fetches and logs the data for 
+// each user ID one by one, in sequence.
+
+
+// You have an asynchronous function performTask() that returns a Promise. 
+// The Promise resolves if the task is successful and rejects if there's an error. 
+// Write a function that calls performTask() and logs a custom success message if the 
+// task is successful, and a custom error message if there's an error.
